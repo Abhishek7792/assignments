8e476fa983298822bdd05bea8a8c0ff5e4666e9e
Abhishek7792 <86602132+Abhishek7792@users.noreply.github.com>
2021-06-29 10:51:42 +0530

    Add files via upload

---
+++ assign1.py
@@ -0,0 +1,10 @@
+a=input("enter first number")
+a=float(a)
+b=input("enter second number")
+b=float(b)
+s=a-b
+p=a*b
+d=a/b
+print("after subtracting",s)
+print("multiplication is",p)
+print("after dividing",d)
