22f84b3ed70a6aedb3508e060bee94454ba7945e
Abhishek7792 <86602132+Abhishek7792@users.noreply.github.com>
2021-07-01 14:06:44 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,33 @@
+#assignment 3
+print(  "1 list") 
+stationary=["pen","paper","pencil","sheets","book"]
+print(stationary)    #this the example of list
+
+print("2 dict")
+
+#dictionary are used to store values
+
+mydict={
+        "book": "contamporary abstract algebra",
+        "writer":"joseph a gallian",
+        "edition":"ninth"
+        }
+print(mydict)
+
+print("3 tuple")
+#collection of python objects separeted by commas
+tup="python","anaconda"
+print(tup)
+
+print("4 sets")
+#sets are defined in python by (i am giving example)
+
+set1={"natural numbers","integers","real numbers"}
+print(set1)
+
+
+print("5 strings in python")
+#example to use strings in python
+
+a="hello this is an example of string" #giving string value to variable a 
+print(a)
