483170841ef7df09d947802167453f8c699cd2fc
Abhishek7792 <86602132+Abhishek7792@users.noreply.github.com>
2021-07-02 12:41:23 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,14 @@
+#assignment4
+
+a=input("enter the altitude at which plan in flying(in feets)")
+a=float(a)
+if(a<=1000):
+    print("safe to land")
+else:
+    if(a>1000 and a<5000):
+        print("bring down to 1000")
+    else:
+        if(a>5000):
+            
+            print("go around and try again")
+        
